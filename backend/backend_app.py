641d44747adb6c2fd2f60a59e37517b7885e316a
MaddinPython <martin.suffa@gmail.com>
2026-04-22 11:43:44 +0200

    added CREATE-endpoint

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -8,6 +8,22 @@ POSTS = [
     {"id": 1, "title": "First post", "content": "This is the first post."},
     {"id": 2, "title": "Second post", "content": "This is the second post."},
 ]
+
+@app.route('/api/posts', methods=['POST'])
+def add_post():
+    new_post = request.get_json()
+    if "title" not in new_post and "content" not in new_post:
+        return jsonify({"error": "'title' and 'content' missing"}), 400
+    elif "title" not in new_post:
+        return jsonify({"error": "'title' missing"}), 400
+    elif "content" not in new_post:
+        return jsonify({"error": "'content' missing"}), 400
+    else:
+        new_post['id'] = max(post['id'] for post in POSTS) + 1 or 1
+        POSTS.append(new_post)
+        return jsonify(new_post), 201
+
+
 
 
 @app.route('/api/posts', methods=['GET'])
